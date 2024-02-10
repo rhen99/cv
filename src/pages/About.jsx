93742b2383def687005ce5f41c1e051fd1a31eb4
Arhen Santiago <lincoldarhen9900@gmail.com>
2024-02-10 18:52:59 +0800

    add featured project Found something I could add to the featured projects sections.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import FProject1 from "../assets/FProject1.png";
 function About() {
   return (
     <main className="text-center lg:text-start lg:w-3/4">
@@ -30,17 +31,43 @@ function About() {
       </section>
       <section className="py-10 min-h-96">
         <h1 className="text-3xl lg:text-5xl lg:pb-10 font-bold pb-5">
-          Best Videos
+          Project Showcase
         </h1>
-        <div className="min-h-40">
-          <h3 className="text-xl font-bold bg-yellow-100 border px-10 py-5 rounded">
-            Coming Soon
-          </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pt-5 lg:pt-0 text-start">
+          <div className="border border-slate-500 rounded">
+            <div
+              className="h-60 bg-slate-300 bg-center bg-cover bg-no-repeat"
+              style={{
+                backgroundImage: `url("${FProject1}")`,
+              }}
+            ></div>
+            <div className="p-5">
+              <a
+                href="https://grid-marking-game.netlify.app/"
+                className="underline hover:text-blue-600"
+              >
+                <h1 className="text-2xl font-bold pt-5">Color Wars</h1>
+              </a>
+
+              <p className="text-lg leading-relaxed">
+                This is a game I made based on a flash game called Blob Wars.
+                It's quite crude looking but it has quite the complexity.
+              </p>
+              <a
+                href="https://github.com/rhen99/React-Marking-Game"
+                className="underline font-bold hover:text-blue-600"
+              >
+                View Repository
+              </a>
+            </div>
+          </div>
+          <div className="border border-slate-300 rounded"></div>
+          <div className="border border-slate-300 rounded"></div>
         </div>
       </section>
       <section className="py-10 min-h-96">
         <h1 className="text-3xl lg:text-5xl lg:pb-10 font-bold pb-5">
-          Project Showcase
+          Best Videos
         </h1>
         <div className="min-h-40">
           <h3 className="text-xl font-bold bg-yellow-100 border px-10 py-5 rounded">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,10 @@
 import Project1 from "../assets/Project1.png";
 import Project2 from "../assets/Project2.png";
 import Project3 from "../assets/Project3.png";
+import Project4 from "../assets/Project4.png";
+import Project5 from "../assets/Project5.png";
+import Project6 from "../assets/Project6.png";
+import FProject1 from "../assets/FProject1.png";
 import { useEffect, useState } from "react";
 function Projects() {
   const [repos, setRepos] = useState([]);
@@ -18,11 +22,37 @@ function Projects() {
         <h1 className="text-3xl lg:text-5xl lg:pb-10 font-bold">
           Featured Projects
         </h1>
-        <p className="text-2xl pb-8">Here's where the big boys are gonna go.</p>
-        <div className="min-h-40">
-          <h3 className="text-xl font-bold bg-yellow-100 border px-10 py-5 rounded text-start">
-            Coming Soon
-          </h3>
+        <p className="text-2xl pb-8">
+          Here are the projects I'm quite proud of.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 pt-5 lg:pt-0 text-start">
+          <div className="border border-slate-500 rounded">
+            <div
+              className="h-60 bg-slate-300 bg-center bg-cover bg-no-repeat"
+              style={{
+                backgroundImage: `url("${FProject1}")`,
+              }}
+            ></div>
+            <div className="p-5">
+              <a
+                href="https://grid-marking-game.netlify.app/"
+                className="underline hover:text-blue-600"
+              >
+                <h1 className="text-2xl font-bold pt-5">Color Wars</h1>
+              </a>
+
+              <p className="text-lg leading-relaxed">
+                This is a game I made based on a flash game called Blob Wars.
+                It's quite crude looking but it has quite the complexity.
+              </p>
+              <a
+                href="https://github.com/rhen99/React-Marking-Game"
+                className="underline font-bold hover:text-blue-600"
+              >
+                View Repository
+              </a>
+            </div>
+          </div>
         </div>
       </section>
       <section className="py-10 min-h-96">
@@ -101,6 +131,86 @@ function Projects() {
             </p>
             <a
               href="https://github.com/rhen99/Todo-List-With-JavaScript-2022"
+              className="underline font-bold hover:text-blue-600"
+            >
+              View Repository
+            </a>
+          </div>
+          <div className="border border-slate-500 rounded p-5">
+            <div
+              className="h-60 bg-slate-300 bg-center bg-cover bg-no-repeat border border-slate-500 rounded"
+              style={{
+                backgroundImage: `url("${Project4}")`,
+              }}
+            ></div>
+            <a
+              href="https://reactcm.netlify.app/"
+              className="underline hover:text-blue-600"
+            >
+              <h1 className="text-2xl font-bold pt-5">
+                Basic Customer Manager App
+              </h1>
+            </a>
+
+            <p className="text-lg leading-relaxed">
+              A basic customer manager app that can take in names, emails, phone
+              numbers, and even a social media username. It uses Google OAuth as
+              sign in.
+            </p>
+            <a
+              href="https://github.com/rhen99/ReactCM"
+              className="underline font-bold hover:text-blue-600"
+            >
+              View Repository
+            </a>
+          </div>
+          <div className="border border-slate-500 rounded p-5">
+            <div
+              className="h-60 bg-slate-300 bg-center bg-cover bg-no-repeat border border-slate-500 rounded"
+              style={{
+                backgroundImage: `url("${Project5}")`,
+              }}
+            ></div>
+            <a
+              href="https://vanillajsweightconverter.netlify.app/"
+              className="underline hover:text-blue-600"
+            >
+              <h1 className="text-2xl font-bold pt-5">
+                Weight Measurement Converter
+              </h1>
+            </a>
+
+            <p className="text-lg leading-relaxed">
+              It's a simple app that will convert one measurement to another. It
+              can convert from as little as ounces all the way to tons.
+            </p>
+            <a
+              href="https://github.com/rhen99/Vanilla-JS-Weight-Converter"
+              className="underline font-bold hover:text-blue-600"
+            >
+              View Repository
+            </a>
+          </div>
+          <div className="border border-slate-500 rounded p-5">
+            <div
+              className="h-60 bg-slate-300 bg-center bg-cover bg-no-repeat border border-slate-500 rounded"
+              style={{
+                backgroundImage: `url("${Project6}")`,
+              }}
+            ></div>
+            <a
+              href="https://chesscomuserfinder.netlify.app/"
+              className="underline hover:text-blue-600"
+            >
+              <h1 className="text-2xl font-bold pt-5">Chess.com User Finder</h1>
+            </a>
+
+            <p className="text-lg leading-relaxed">
+              An app where you can see the chess.com leaderboards, in all time
+              control.
+            </p>
+            <a
+              href="https://github.com/rhen99/Chesscom-User-Finder"
               className="underline font-bold hover:text-blue-600"
             >
               View Repository
